3f5336cf3f978cc5f71e0b36e23f94363a73f359
ayan8653962 <ayan8653962@gmail.com>
2025-09-21 01:46:00 +0500

    practice js

---
+++ app.js
@@ -0,0 +1,27 @@
+for (var i=1; i<=10;  i++){
+    document.writeln("Number: " , i , "<br>");
+}
+for(i=5; i<=15; i++){
+    document.writeln(i , "<br>")
+}
+for (i=10; i>=0; i--){
+    document.writeln(i , "<br>")
+
+}
+for(var i=2; i<=30; i+=2){
+    document.writeln(i , "<br>")
+}
+var detail=("Zeeshan Abbasi")
+console.log(detail)
+
+
+
+
+
+
+
+
+
+
+
+
